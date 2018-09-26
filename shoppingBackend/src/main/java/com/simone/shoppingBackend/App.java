593092f1d694db907545e87174efc01d1977d093
Simone <simone.pieretti25@gmail.com>
2018-09-26 11:53:16 +0200

    My project setup completed successfully

---
+++ shoppingBackend/src/main/java/com/simone/shoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.simone.shoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
